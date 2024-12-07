480005bee4ed7cc45b3c0b667e2737266d6fe720
João Sebastião Capindissa <43992006+0laf3@users.noreply.github.com>
2024-12-07 17:40:13 +0100

    `Update Firebase configuration and initialization in app.js`

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,22 @@
-// Initialize Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDdrnzNFFQzQkter9qMn5qeTRCspsxmvJk",
+  authDomain: "movie-app-f2790.firebaseapp.com",
+  databaseURL: "https://movie-app-f2790-default-rtdb.firebaseio.com",
+  projectId: "movie-app-f2790",
+  storageBucket: "movie-app-f2790.firebasestorage.app",
+  messagingSenderId: "210502729839",
+  appId: "1:210502729839:web:64aee2051aa5719cf40d9a",
+  measurementId: "G-PBGX4D1SPG"
 };
 
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
